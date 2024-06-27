cb27f3486f7cc8845f4ca562b9929efa18631fb4
2022-24185 <2022-24185@snu.ac.kr>
2024-06-27 19:18:53 +0900

    Replaced the old manager class with experiment, networker and phenotypecreator

--- neuroevolution/server/tasks.py
+++ neuroevolution/server/tasks.py
@@ -1,7 +1,8 @@
 # neuroevolution/server/tasks.py
 import logging
-from neuroevolution.evolution.manager import EvolutionManager
 from typing import TYPE_CHECKING
+from neuroevolution.evolution.networker import Network
+from neuroevolution.evolution.experiment import Experiment
 from neuroevolution.server.errors import MissingGenomeError
 from neuroevolution.server.data_storage import SessionData
 
@@ -12,35 +13,35 @@ if TYPE_CHECKING:
 
 logging.basicConfig(level=logging.INFO)
 
-evolution_manager = EvolutionManager('neuroevolution/evolution/config_cppn_social_brain')
+experiment = Experiment('neuroevolution/evolution/config_cppn_social_brain')
+network = Network(experiment)
 session_data = SessionData('session_data.csv')
 
 def process_user_data(data: 'UserData'):
     logging.info(f"Processing user data: {data}")
     session_data.store_session_data(data)
-    evolution_manager.receive_evaluation(data)
+    network.receive_evaluation(data)
     logging.info(f"User data processed: {data}")
 
 def swap_out_mediator(user_data: 'UserData') -> bytes:
     logging.info(f"Requesting new mediator genome for mediator ID: {user_data.genome_id}")
     session_data.store_session_data(user_data)
     logging.info(f"User data stored: {user_data}")
-    evolution_manager.receive_evaluation(user_data)
+    network.receive_evaluation(user_data)
     logging.info(f"User data evaluated: {user_data}"[:100])
-    new_genome = evolution_manager.fetch_new_genome()
-    if not new_genome:
+    new_mediator = network.get_serialized_mediator()
+    if not new_mediator:
         logging.error("Failed to fetch new genome")
         raise MissingGenomeError("Failed to fetch new genome")
-    new_mediator = evolution_manager.get_serialized_network(new_genome)
     logging.info(f"New mediator provided.")
     return new_mediator
 
 def run_evolution():
     logging.info("Starting the evolutionary process")
-    evolution_manager.advance_population()
+    experiment.start()
     logging.info("Evolutionary process finished")
 
 def reset_population():
     logging.info("Restarting the population")
-    evolution_manager.reset()
+    experiment.reset()
     logging.info("Population restarted")

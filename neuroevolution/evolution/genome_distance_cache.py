4b1a22d3fe96d2a13f42251eeab14ac3165c90af
2022-24185 <2022-24185@snu.ac.kr>
2024-06-27 17:38:11 +0900

    Separated out genomedistancecache

---
+++ neuroevolution/evolution/genome_distance_cache.py
@@ -0,0 +1,23 @@
+from neat.genome import DefaultGenome
+
+class GenomeDistanceCache(object):
+    def __init__(self, config):
+        self.config = config
+        self.distances = {}
+        self.hits = 0
+        self.misses = 0
+
+    def __call__(self, genome0: DefaultGenome, genome1: DefaultGenome):
+        g0 = genome0.key
+        g1 = genome1.key
+        d = self.distances.get((g0, g1))
+        if d is None:
+            # Distance is not already computed.
+            d = genome0.distance(genome1, self.config)
+            self.distances[g0, g1] = d
+            self.distances[g1, g0] = d
+            self.misses += 1
+        else:
+            self.hits += 1
+
+        return d
